09503b113324f2e668b34b8896fede3d2b336712
Rai Araujo <raisanhacu@gmail.com>
2024-07-09 14:00:10 -0300

    Criando o exercicio

---
+++ Exercicios/Classes/Bolinhas/script.js
@@ -0,0 +1,10 @@
+const idnum_objetos =document.getElementById("idnum_objetos")
+const txt_qtde = document.getElementById("txt_qtde")
+const btn_add = document.getElementById("btn_add")
+const btn_remover = document.getElementById("btn_remover")
+const palco = document.getElementById("palco")
+
+let larguraPalco = palco.offsetWidth
+let alturaPalco = palco.offsetHeight
+let bolas = []
+let numBola = 0
