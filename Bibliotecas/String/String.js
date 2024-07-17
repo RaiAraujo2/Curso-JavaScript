3128a9063056658b10af2c932f98292139becce0
Rai Araujo <raisanhacu@gmail.com>
2024-07-17 10:35:53 -0300

    Metodos String

---
+++ Bibliotecas/String/String.js
@@ -0,0 +1,9 @@
+let nome = "Bruno"
+let canal = "CFBcursos"
+
+console.log(nome.charAt(0))
+console.log(canal.charCodeAt(3))
+console.log(nome.concat(" Dias"))
+console.log(nome)
+nome = nome.concat(" Dias")
+console.log(nome.indexOf("o"))
