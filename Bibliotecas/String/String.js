29a38b1127b45e87dc552ff4cc5b9c63b03131d7
Rai Araujo <raisanhacu@gmail.com>
2024-07-17 10:51:17 -0300

    Aula 03

--- Bibliotecas/String/String.js
+++ Bibliotecas/String/String.js
@@ -15,11 +15,27 @@ console.log(nome.localeCompare(canal))
 
 console.log(nome.replace("Dias", "Pinheiro"))
 
+// search - retorna a posição da string
 console.log(nome.search("Dias"))
 
+// slice(inicio, fim) - retorna uma parte da string
 let sobrenome = nome.slice(6, 10)
 console.log(nome.slice(0, 5))
 console.log(sobrenome)
 
+// split - divide a string em um array
 let arr_nome = nome.split(" ")
 console.log(arr_nome)
+
+// substr - retorna uma parte da string
+console.log(nome.substr(0, 5))
+
+// Transforma para maiusculo e minusculo
+console.log(nome.toLowerCase())
+console.log(nome.toUpperCase())
+
+// Retorna o valor primitivo do objeto, 
+console.log(nome.valueOf())
+
+num = 10
+console.log(typeof(num.toString()))
