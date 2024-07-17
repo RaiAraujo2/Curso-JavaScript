e727f55dd43366408b4fa52f1bcdf07280779c4c
Rai Araujo <raisanhacu@gmail.com>
2024-07-17 10:45:41 -0300

    Aula 02

--- Bibliotecas/String/String.js
+++ Bibliotecas/String/String.js
@@ -1,5 +1,5 @@
-let nome = "Bruno"
-let canal = "CFBcursos"
+let nome = new String("Bruno")
+let canal = new String("Bruno")
 
 console.log(nome.charAt(0))
 console.log(canal.charCodeAt(3))
@@ -7,3 +7,19 @@ console.log(nome.concat(" Dias"))
 console.log(nome)
 nome = nome.concat(" Dias")
 console.log(nome.indexOf("o"))
+
+// retorno = 0: iguais
+// retorno = 1: diferente
+// retorno = -1: diferente
+console.log(nome.localeCompare(canal))
+
+console.log(nome.replace("Dias", "Pinheiro"))
+
+console.log(nome.search("Dias"))
+
+let sobrenome = nome.slice(6, 10)
+console.log(nome.slice(0, 5))
+console.log(sobrenome)
+
+let arr_nome = nome.split(" ")
+console.log(arr_nome)
